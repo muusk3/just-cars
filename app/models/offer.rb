a6128a9bcd2eda65f093b427758422d9a3380c06
Krzysztof Krupski <muusk@icloud.com>
2021-08-05 21:04:54 +0200

    add Offer model validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,9 @@
 class Offer < ApplicationRecord
+  validates :title, :description, presence: true
+  validates :price,
+    presence: true,
+    format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
+    numericality: { in: 0..10_000_000 }
+
+  has_one_attached :photo
 end
