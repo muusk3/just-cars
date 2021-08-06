11698265dc77e4344aa6452e980308f872f277c0
Krzysztof Krupski <muusk@icloud.com>
2021-08-06 16:16:01 +0200

    update seeds.rb
    - add offer generator in random quantity

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Object.const_defined?('Faker')
+  max_offers = rand(30)
+
+  def stream_photo_url
+    Faker::LoremFlickr.image(size: '480x320', search_terms: ['cars'])
+  end
+
+  (1..max_offers).each do |num|
+    offer_args = {
+      title:       format("%<vehicle>s for sale!", vehicle: Faker::Vehicle.make_and_model),
+      description: Faker::Vehicle.standard_specs.join(', '),
+      price:       Faker::Number.decimal(l_digits: 2),
+      photo:       AttachmentFormatterService.data_from_url(stream_photo_url)
+    }
+    Offer.create(offer_args)
+  end
+else
+  puts "'Faker' gem is not installed."
+end
